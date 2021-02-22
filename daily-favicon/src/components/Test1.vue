b9ed03d7779e115f9d9e228a2cd247b17096ad9c
ruzun88 <ruzun88@gmail.com>
2021-02-22 22:14:26 +0900

    1차 테스트

--- daily-favicon/src/components/Test1.vue
+++ daily-favicon/src/components/Test1.vue
@@ -6,7 +6,13 @@
 
 <script>
 export default {
-
+created() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = '/mn-test.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  },
 }
 </script>
 
--- daily-favicon/src/components/Test2.vue
+++ daily-favicon/src/components/Test2.vue
@@ -10,7 +10,7 @@ created() {
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = 'https://www.google.com/favicon.ico';
+    link.href = '/kr-test.png';
     document.getElementsByTagName('head')[0].appendChild(link);
   },
 }
